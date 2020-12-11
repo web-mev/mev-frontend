0e5d2a65b3726f35303c2e490fd25ee255818a08
anastasia-si <nastya484@bk.ru>
2020-12-11 11:05:08 -0500

    fix: fix error with twttr is not defined" in FF

--- projects/web-mev/src/app/features/about/about/about.component.ts
+++ projects/web-mev/src/app/features/about/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'mev-about',
@@ -11,13 +12,37 @@ import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 export class AboutComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   releaseButler = require('../../../../assets/release-butler.png');
+  private twitter: any;
 
-  constructor() {}
+  constructor(private _router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initTwitterWidget();
+  }
 
-  ngAfterViewInit(): void {
-    // display the latest tweets
-    (<any>window).twttr.widgets.load();
+  initTwitterWidget() {
+    this.twitter = this._router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        (<any>window).twttr = (function(d, s, id) {
+          let js: any,
+            fjs = d.getElementsByTagName(s)[0],
+            t = (<any>window).twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = 'https://platform.twitter.com/widgets.js';
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function(f: any) {
+            t._e.push(f);
+          };
+
+          return t;
+        })(document, 'script', 'twitter-wjs');
+
+        if ((<any>window).twttr.ready()) (<any>window).twttr.widgets.load();
+      }
+    });
   }
 }

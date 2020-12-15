a6f575848f5d459f4c35bcf63de74fab95e721ac
anastasia-si <nastya484@bk.ru>
2020-12-15 12:29:45 -0500

    fix: update styles for pop-up windows to fix overlapping elements

--- projects/web-mev/src/app/features/workspace-detail/components/metadata/dialogs/view-set-dialog/view-set-dialog.component.ts
+++ projects/web-mev/src/app/features/workspace-detail/components/metadata/dialogs/view-set-dialog/view-set-dialog.component.ts
@@ -5,6 +5,7 @@ import {
   Inject
 } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CustomSetType } from '@app/_models/metadata';
 
 @Component({
   selector: 'mev-view-set-dialog',
@@ -13,12 +14,17 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewSetDialogComponent implements OnInit {
+  isObservationSet: boolean;
+
   constructor(
     public dialogRef: MatDialogRef<ViewSetDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isObservationSet =
+      this.data.type == CustomSetType.FeatureSet ? false : true;
+  }
 
   onNoClick(): void {
     this.dialogRef.close();

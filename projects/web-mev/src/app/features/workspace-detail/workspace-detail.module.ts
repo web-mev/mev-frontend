ef0f193c759b74333c1b04a2589e7af35a2350e2
anastasia-si <nastya484@bk.ru>
2021-02-14 13:57:40 -0500

    feat: remove unused files and components

--- projects/web-mev/src/app/features/workspace-detail/workspace-detail.module.ts
+++ projects/web-mev/src/app/features/workspace-detail/workspace-detail.module.ts
@@ -16,14 +16,13 @@ import { MetadataComponent } from './components/metadata/metadata.component';
 import { RouterModule } from '@angular/router';
 import { AddAnnotationDialogComponent } from './components/metadata/dialogs/add-annotation-dialog/add-annotation-dialog.component';
 import { AddObservationSetDialogComponent } from './components/metadata/dialogs/add-observation-set-dialog/add-observation-set-dialog.component';
-import { AddFeatureSetDialogComponent } from './components/metadata/dialogs/add-feature-set-dialog/add-feature-set-dialog.component';
 import { DeleteSetDialogComponent } from './components/metadata/dialogs/delete-set-dialog/delete-set-dialog.component';
 import { ViewSetDialogComponent } from './components/metadata/dialogs/view-set-dialog/view-set-dialog.component';
-import { InlineEditComponent } from './components/metadata/inline-edit/inline-edit.component';
 import { SatPopoverModule } from '@ncstate/sat-popover';
 import { AnalysisModule } from '../analysis/analysis.module';
 import { EditFeatureSetDialogComponent } from './components/metadata/dialogs/edit-feature-set-dialog/edit-feature-set-dialog.component';
 import { EditDialogComponent } from './components/dialogs/edit-dialog/edit-dialog/edit-dialog.component';
+import { ViewInfoDialogComponent } from './components/metadata/dialogs/view-info-dialog/view-info-dialog.component';
 
 @NgModule({
   declarations: [
@@ -37,12 +36,11 @@ import { EditDialogComponent } from './components/dialogs/edit-dialog/edit-dialo
     MetadataComponent,
     AddAnnotationDialogComponent,
     AddObservationSetDialogComponent,
-    AddFeatureSetDialogComponent,
     DeleteSetDialogComponent,
     ViewSetDialogComponent,
-    InlineEditComponent,
     EditFeatureSetDialogComponent,
-    EditDialogComponent
+    EditDialogComponent,
+    ViewInfoDialogComponent
   ],
   exports: [],
   imports: [

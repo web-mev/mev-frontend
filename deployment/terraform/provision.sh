78652d96796ac3ef70dd867e1b4d4d9fb1681901
blawney <blawney@hsph.harvard.edu>
2021-05-10 08:55:17 -0400

    Fixing issue where auth token was not modified to point at a dynamic api endpoint. Removing the hardcoded files which pointed to the old static IP serving http

--- deployment/terraform/provision.sh
+++ deployment/terraform/provision.sh
@@ -15,6 +15,7 @@ API_ENDPOINT=${backend_url}/api
 cd mev-frontend || exit 1
 /usr/bin/npm install
 
+# Depending on the deployment environment, fill-in the appropriate environment.ts file
 if [ $ENVIRONMENT = 'prod' ]; then
     sed -e 's?__API_URL__?'"$API_ENDPOINT"'?g' projects/web-mev/src/environments/environment.prod.ts.tmpl > projects/web-mev/src/environments/environment.prod.ts
     /usr/bin/npm run build --configuration=production
@@ -22,5 +23,8 @@ else
     sed -e 's?__API_URL__?'"$API_ENDPOINT"'?g' projects/web-mev/src/environments/environment.ts.tmpl > projects/web-mev/src/environments/environment.ts
     /usr/bin/npm run build
 fi
+
+# Regardless of the deployment environment, point the JWT file to the API
+sed -e 's?__API_URL__?'"$API_ENDPOINT"'?g' projects/web-mev/src/app/jwtConfig.ts.tmpl > projects/web-mev/src/app/jwtConfig.ts
 
 /usr/bin/cp -a dist/web-mev/* /var/www/html

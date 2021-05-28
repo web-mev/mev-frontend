585af6c6ba0dc989bade68084fc3c5dc7713ee15
blawney <blawney@hsph.harvard.edu>
2021-05-28 15:09:35 -0400

    A few lingering variants on WebMeV

--- projects/web-mev/src/environments/environment.test.ts
+++ projects/web-mev/src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 const packageJson = require('../../../../package.json');
 
 export const environment = {
-  appName: 'Web MeV',
+  appName: 'WebMeV',
   envName: 'TEST',
   production: false,
   test: true,

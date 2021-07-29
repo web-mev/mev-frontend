0b97068c00a2aaf2e829f3240d520d66bf0dc109
Astha Flynn <aflynn@g.harvard.edu>
2021-07-29 02:24:20 +0000

    Save observation sets, mouse tooltip, toggle table/donut views, donut legend

--- projects/web-mev/src/app/d3/components/sctk-doublet-detection/sctk-doublet-detection.component.ts
+++ projects/web-mev/src/app/d3/components/sctk-doublet-detection/sctk-doublet-detection.component.ts
@@ -19,7 +19,7 @@ import { tap, finalize } from 'rxjs/operators';
 import { MetadataService } from '@app/core/metadata/metadata.service';
 import { NotificationService } from '../../../core/core.module';
 import { AddSampleSetComponent } from '@app/d3/components/dialogs/add-sample-set/add-sample-set.component';
-import { CustomSetType } from '@app/_models/metadata';
+import { CustomSet, CustomSetType } from '@app/_models/metadata';
 import { DataSource } from '@angular/cdk/table';
 
 @Component({
@@ -40,8 +40,12 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
   imageName = 'doublet_detection';
   analysisName = 'SCTK Doublet Finder';
 
+  selectedSamples = [];
+  customObservationSets = [];
+
   /* Chart settings */
   containerId = '#donutPlot';
+  chartViewMode = true;
   precision = 2;
   margin = { top: 50, right: 300, bottom: 50, left: 70 }; // chart margins
   outerHeight = 500;
@@ -58,9 +62,9 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
   displayColumns; // will be the concatenation of static + dynamic cols
 
   string_operators = [
-    { id: 'startswith', name: ' Starts with: '},
-    { id: 'case-ins-eq', name: ' = (case-insensitive) '},
-    { id: 'eq', name: ' = (strict) '}
+    { id: 'startswith', name: ' Starts with: ' },
+    { id: 'case-ins-eq', name: ' = (case-insensitive) ' },
+    { id: 'eq', name: ' = (strict) ' }
   ];
 
   defaultPageIndex = 0;
@@ -88,7 +92,7 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
     public dialog: MatDialog,
     private metadataService: MetadataService,
     private readonly notificationService: NotificationService
-  ){
+  ) {
     this.dataSource = new ExpressionMatrixDataSource(this.analysesService);
     // adding form controls depending on the tables settings (the allowedFilters property)
     for (const key in this.allowedFilters) {
@@ -146,12 +150,11 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
   }
 
   initData(): void {
-    console.log('init data')
-    console.log(this.outputs)
+    console.log('init data');
+    console.log(this.outputs);
     // this.resourceId = this.outputs.qc_results;
-    this.resourceId = this.outputs['SctkDoubletFinder.doublet_ids']
-    console.log(this.resourceId)
-
+    this.resourceId = this.outputs['SctkDoubletFinder.doublet_ids'];
+    console.log(this.resourceId);
 
     const sorting = {
       sortField: this.defaultSorting.field,
@@ -167,20 +170,18 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
       this.defaultPageIndex,
       this.defaultPageSize
     );
-    this.dataSource.sampleNames$.subscribe( data => {
-        this.dynamicColumns = data
-        this.displayColumns = [...this.staticCols, ...data];
-      }
-    );
-    console.log('init data log of datasource')
-    console.log(this.dataSource)
-
+    this.dataSource.sampleNames$.subscribe(data => {
+      this.dynamicColumns = data;
+      this.displayColumns = [...this.staticCols, ...data];
+    });
+    console.log('init data log of datasource');
+    console.log(this.dataSource);
   }
 
   /**
    * Function to construct the parameter filters that are passed to the backend
    */
-  createFilters(){
+  createFilters() {
     const formValues = this.filterForm.value; // i.e. {name: "asdfgh", doublet_class: "doublet"}
     const paramFilter = {}; // has values {'log2FoldChange': '[absgt]:2'};
     for (const key in this.allowedFilters) {
@@ -216,8 +217,8 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
       this.paginator.pageIndex,
       this.paginator.pageSize
     );
-    console.log('log from load page')
-    console.log(this.dataSource)
+    console.log('log from load page');
+    console.log(this.dataSource);
   }
 
   /**
@@ -233,15 +234,14 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
    * Function that is triggered when the user clicks the "Create a custom sample" button
    */
   onCreateCustomFeatureSet() {
-
     // We don't want to create exceptionally large feature sets. Check
     // that they don't exceed some preset size
     const setSize = this.dataSource.qcCount;
-    if (setSize > this.maxFeatureSetSize){
+    if (setSize > this.maxFeatureSetSize) {
       const errorMessage = `The current size of
         your set (${setSize}) is larger than the
         maximum allowable size (${this.maxFeatureSetSize}).
-        Please filter the table further to reduce the size.`
+        Please filter the table further to reduce the size.`;
       this.notificationService.error(errorMessage);
       return;
     }
@@ -256,16 +256,10 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
         // page in the table.
         const filterValues = this.createFilters();
         this.analysesService
-          .getResourceContent(
-            this.resourceId,
-            null,
-            null,
-            filterValues,
-            {}
-          )
+          .getResourceContent(this.resourceId, null, null, filterValues, {})
           .subscribe(features => {
             const elements = features.map(feature => {
-              return {id: feature.rowname};
+              return { id: feature.rowname };
             });
             const customSet = {
               name: customSetData.name,
@@ -287,6 +281,18 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
     this.createChart();
   }
 
+  /**
+   * Function is triggered when switching between Chart and Table view modes
+   */
+  onChartViewChange(chartViewEvent) {
+    if (chartViewEvent === 'plotMode') {
+      this.chartViewMode = true;
+    }
+    if (chartViewEvent === 'tableMode') {
+      this.chartViewMode = false;
+    }
+  }
+
   createChart(): void {
     const outerWidth = this.svgElement.nativeElement.offsetWidth;
     const outerHeight = this.outerHeight;
@@ -301,9 +307,9 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
     //   a[doublet_class] += 1;
     //   return a;
     // }, {});
-    const data = {singlet:243, doublet:53}
-    console.log("donut data")
-    console.log(data)
+    const data = { singlet: 243, doublet: 53 };
+    console.log('donut data');
+    console.log(data);
     d3.select(this.containerId)
       .selectAll('svg')
       .remove();
@@ -314,31 +320,134 @@ export class SctkDoubletDetectionComponent implements OnInit, AfterViewInit {
       .attr('width', width)
       .attr('height', height)
       .append('g')
-      .attr(
-        'transform',
-        'translate(' + width / 2 + ',' + height / 2 + ')'
-      )
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-    const color = d3.scaleOrdinal()
-      .range(["#98abc5", "#8a89a6"]);
+    const color = d3.scaleOrdinal().range(['#193A6F', '#F98125']);
 
-    const pie = d3.pie()
-      .value(d=>d[1]);
-    const data_ready = pie(Object.entries(data))
+    const pie = d3.pie().value(d => d[1]);
+    const data_ready = pie(Object.entries(data));
+
+    const donutTip = d3
+      .select(this.containerId)
+      .append('div')
+      .attr('class', 'donut-tip')
+      .style('opacity', 0)
+      .style('background', '#FFFFFF')
+      .style('color', '#313639')
+      .style('padding', '0.5rem')
+      .style('border', '1px solid #313639')
+      .style('border-radius', '8px')
+      .style('width', '60px')
+      .style('position', 'absolute')
+      .style('text-align', 'center')
+      .style('font-weight', 'bold');
 
     // // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
     svg
-      .selectAll('whatever')
+      .selectAll('allSlices')
       .data(data_ready)
       .join('path')
-      .attr('d', d3.arc()
-        .innerRadius(100)         // This is the size of the donut hole
-        .outerRadius(radius)
+      .attr(
+        'd',
+        d3
+          .arc()
+          .innerRadius(100) // This is the size of the donut hole
+          .outerRadius(radius)
       )
       .attr('fill', d => color(d.data[0]))
-      .attr("stroke", "black")
-      .style("stroke-width", "2px")
-      .style("opacity", 0.7)
+      .attr('stroke', 'black')
+      .style('stroke-width', '2px')
+      .on('mouseover', function(event, d) {
+        d3.select(this)
+          .transition()
+          .duration(50)
+          .attr('opacity', 0.7);
+        donutTip
+          .transition()
+          .duration(50)
+          .style('opacity', 1);
+        let num =
+          Math.round(
+            (d.data[1] / (data_ready[0].data[1] + data_ready[1].data[1])) * 100
+          ).toString() + '%';
+        donutTip
+          .html(num)
+          .style('left', width / 2 + 'px')
+          .style('top', height / 2 + 'px');
+      })
+      .on('mouseout', function(event, d) {
+        d3.select(this)
+          .transition()
+          .duration(50)
+          .attr('opacity', 1);
+        donutTip
+          .transition()
+          .duration(50)
+          .style('opacity', 0);
+      });
+
+    const legendRectSize = 13;
+    const legendSpacing = 7;
+    const legend = svg
+      .selectAll('.legend')
+      .data(color.domain())
+      .enter()
+      .append('g')
+      .attr('class', 'circle-legend')
+      .attr('transform', function(d, i) {
+        const height = legendRectSize + legendSpacing;
+        const offset = (height * color.domain().length) / 2;
+        const horz = -2 * legendRectSize - 13;
+        const vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+      });
+
+    legend
+      .append('circle')
+      .style('fill', color)
+      .style('stroke', color)
+      .attr('cx', 0)
+      .attr('cy', 0)
+      .attr('r', '.5rem');
+
+    legend
+      .append('text')
+      .attr('x', legendRectSize + legendSpacing)
+      .attr('y', legendRectSize - legendSpacing)
+      .text(function(d) {
+        return d;
+      });
+  }
+
+  /**
+   * Function that is triggered when the user clicks the "Create a custom sample" button
+   */
+  onCreateCustomSampleSet() {
+    const dialogRef = this.dialog.open(AddSampleSetComponent);
+
+    dialogRef.afterClosed().subscribe(customSetData => {
+      if (customSetData) {
+        const filterValues = this.createFilters();
+        this.analysesService
+          .getResourceContent(this.resourceId, null, null, filterValues, {})
+          .subscribe(features => {
+            const elements = features.map(feature => {
+              return { id: feature.rowname };
+            });
+            const observationSet: CustomSet = {
+              name: customSetData.name,
+              color: customSetData.color,
+              type: CustomSetType.ObservationSet,
+              elements: elements,
+              multiple: true
+            };
+
+            if (this.metadataService.addCustomSet(observationSet)) {
+              this.customObservationSets = this.metadataService.getCustomObservationSets();
+            }
+          });
+      }
+    });
   }
 }
 
@@ -347,7 +456,8 @@ export interface QCDoubletDetection {
   doublet_class: string;
 }
 
-export class ExpressionMatrixDataSource implements DataSource<QCDoubletDetection> {
+export class ExpressionMatrixDataSource
+  implements DataSource<QCDoubletDetection> {
   public qcSubject = new BehaviorSubject<QCDoubletDetection[]>([]);
   public qcCount = 0;
   private sampleNamesSubject = new BehaviorSubject<string[]>([]);
@@ -374,19 +484,16 @@ export class ExpressionMatrixDataSource implements DataSource<QCDoubletDetection
         sorting
       )
       .pipe(finalize(() => this.loadingSubject.next(false)))
-      .subscribe(
-        data => {
-          this.qcCount = data.count;
-          const qcFormatted = data.results.map(
-            dataRow => {
-              return {
-                name: dataRow.rowname,
-                doublet_class: dataRow.values["doublet_class"]
-              };
-            }
-          );
-          return this.qcSubject.next(qcFormatted);
+      .subscribe(data => {
+        this.qcCount = data.count;
+        const qcFormatted = data.results.map(dataRow => {
+          return {
+            name: dataRow.rowname,
+            doublet_class: dataRow.values['doublet_class']
+          };
         });
+        return this.qcSubject.next(qcFormatted);
+      });
   }
 
   connect(): Observable<QCDoubletDetection[]> {
@@ -398,6 +505,3 @@ export class ExpressionMatrixDataSource implements DataSource<QCDoubletDetection
     this.loadingSubject.complete();
   }
 }
-
-
-

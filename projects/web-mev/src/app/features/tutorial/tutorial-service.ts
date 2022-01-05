58efe11f8f0deceb41d7de54600e9af9d67257c8
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-01-05 14:17:43 -0500

    Adding buttons to directly add tutorial files to a user account

---
+++ projects/web-mev/src/app/features/tutorial/tutorial-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+
+/**
+ * Used for adding the tutorial files to the user's files
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialService {
+  private readonly API_URL = environment.apiUrl;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  /**
+   * Add file, post method for Dropbox upload
+   *
+   */
+  addTutorialFile(payload: any): Observable<any> {
+    return this.httpClient
+      .post(`${this.API_URL}/resources/add-bucket-resources/`, payload)
+  }
+}

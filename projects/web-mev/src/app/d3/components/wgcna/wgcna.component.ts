736208990bea3ee68f99846cf8e20a3a4b8abf5e
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-01-07 11:06:21 -0500

    wip: Implemented basic table for wgcna output.

--- projects/web-mev/src/app/d3/components/wgcna/wgcna.component.ts
+++ projects/web-mev/src/app/d3/components/wgcna/wgcna.component.ts
@@ -2,12 +2,25 @@ import {
   Component,
   ChangeDetectionStrategy,
   Input,
-  OnInit
+  OnInit,
+  ViewChild,
+  AfterViewInit
 } from '@angular/core';
+import { BehaviorSubject, Observable, merge } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
+import { DataSource } from '@angular/cdk/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { AnalysesService } from '@app/features/analysis/services/analysis.service';
 
+export interface WGCNAModule {
+  module_name: string;
+  module_genes: string[];
+  module_size: number;
+}
 
 /**
- * Used for Combat-Seq output
+ * Used for WGCNA output where we have both gene modules
+ * and a QC plot
  */
 @Component({
   selector: 'mev-wgcna',
@@ -15,14 +28,107 @@ import {
   styleUrls: ['./wgcna.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default
 })
-export class WgcnaComponent implements OnInit {
+export class WgcnaComponent implements OnInit, AfterViewInit {
   @Input() outputs;
-  analysisName = 'WGCNA';
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() {}
+  analysisName = 'WGCNA';
+  dataSource: WGCNADataSource;
+
+  displayedColumns = ['module_id','module_size', 'actions'];
+  defaultPageIndex = 0;
+  defaultPageSize = 10;
+
+  constructor(
+    private analysesService: AnalysesService,
+  ) {
+    this.dataSource = new WGCNADataSource(this.analysesService);
+  }
 
   ngOnInit() {
-    console.log('on init...');
+    this.fetchData();
   }
-  
+
+  fetchData() {
+   this.dataSource.loadData(
+     this.outputs['module_results'],
+     {},
+     {},
+     this.defaultPageIndex,
+     this.defaultPageSize
+     );
+  }
+
+  ngAfterViewInit() {
+
+    this.paginator.page.subscribe(
+      () => {
+        this.dataSource.loadData(
+          this.outputs['module_results'],
+          {},
+          {},
+          this.paginator.pageIndex,
+          this.paginator.pageSize
+          );
+      }
+    );
+  }
+
+  createFeatureSet(row){
+    console.log('Create FS!');
+    console.log(row);
+  } 
+}
+
+
+export class WGCNADataSource implements DataSource<WGCNAModule> {
+  public modulesSubject = new BehaviorSubject<WGCNAModule[]>([]);
+  public modulesCount = 0;
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loadingSubject.asObservable();
+
+  constructor(private analysesService: AnalysesService) {}
+
+  loadData(
+    resourceId: string,
+    filterValues: object,
+    sorting: object,
+    pageIndex: number,
+    pageSize: number
+  ) {
+    this.loadingSubject.next(true);
+
+    this.analysesService
+      .getResourceContent(
+        resourceId,
+        pageIndex + 1,
+        pageSize,
+        filterValues,
+        sorting
+      )
+      .pipe(finalize(() => this.loadingSubject.next(false)))
+      .subscribe(response => {
+        this.modulesCount = response.count;
+        let reformattedData = response.results.map(
+          r => {
+            let n = r.genes.length;
+            return {
+              module_name: r.module,
+              module_genes: r.genes,
+              module_size: n
+            }
+          }
+        );
+        return this.modulesSubject.next(reformattedData);
+      });
+  }
+
+  connect(): Observable<WGCNAModule[]> {
+    return this.modulesSubject.asObservable();
+  }
+
+  disconnect(): void {
+    this.modulesSubject.complete();
+    this.loadingSubject.complete();
+  }
 }

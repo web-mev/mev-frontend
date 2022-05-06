2c59a9b3ab33c788f30561e9da394764853cb181
Saron Nhong <saronnhong@gmail.com>
2022-05-06 16:04:41 -0700

    able to create new datasets with filters on target and tcga

--- projects/web-mev/src/app/features/public-datasets-manager/components/public-datasets-container/public-datasets.component.ts
+++ projects/web-mev/src/app/features/public-datasets-manager/components/public-datasets-container/public-datasets.component.ts
@@ -13,7 +13,7 @@ import { environment } from '@environments/environment';
 export class PublicDatasetsComponent implements OnInit {
   private readonly API_URL = environment.apiUrl;
   currentDataset = '';
-  queryString: string;
+  queryString: string = '';
   filterItems = {};
   facetField;
   searchQueryResults: string = "";
@@ -22,19 +22,39 @@ export class PublicDatasetsComponent implements OnInit {
 
   // targetFields = ["ethnicity", "gender"]
   targetFields = ["ethnicity", "gender", "race", "vital_status", "cog_renal_stage", "last_known_disease_status", "morphology", "primary_diagnosis", "progression_or_recurrence", "site_of_resection_or_biopsy", "tissue_or_organ_of_origin", "tumor_grade", "dbgap_accession_number", "disease_type", "name", "primary_site", "project_id"];
-  targetRangeFields = ["age_at_diagnosis", "days_to_last_follow_up", "year_of_diagnosis"]
+  targetRangeFields = ["age_at_diagnosis", "days_to_last_follow_up", "year_of_diagnosis"];
+  tcgaFields = ["alcohol_history", "ethnicity", "gender", "race", "vital_status", "vital_status", "ajcc_pathologic_m", "ajcc_pathologic_n", "ajcc_pathologic_stage", "ajcc_pathologic_t", "ajcc_staging_system_edition", "classification_of_tumor", "days_to_diagnosis", "icd_10_code", "last_known_disease_status", "morphology", "primary_diagnosis", "prior_malignancy", "prior_treatment", "progression_or_recurrence", "site_of_resection_or_biopsy", "synchronous_malignancy", "tissue_or_organ_of_origin", "tumor_grade", "disease_type", "name", "primary_site", "project_id"];
+  tcgaRangeFields = ["age_at_diagnosis", "age_at_index", "days_to_birth", "days_to_last_follow_up", "year_of_birth", "year_of_diagnosis"];
+  filterFields = [];
 
   constructor(fb: FormBuilder, private httpClient: HttpClient, private ref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    let currDataSet = 'target-rnaseq';
-    this.queryString = this.buildFacetFieldQueryString(this.targetFields, currDataSet);
+
+    // let currDataSet = 'target-rnaseq';
+    // if (currDataSet.length > 0) {
+    //   this.queryString = this.buildFacetFieldQueryString(this.targetFields, currDataSet);
+    //   this.updateFilterValues();
+    // }
+
 
     // this.buildFacetFieldQueryRangeString(this.targetRangeFields[0], currDataSet, 0, 7000, 1000)
     // this.buildFacetFieldQueryRangeString(this.targetRangeFields[1], currDataSet, 0, 4000, 500)
     // this.buildFacetFieldQueryRangeString(this.targetRangeFields[2], currDataSet, 1980, 2022, 10)
+  }
 
-    this.updateFilterValues()
+  afterLoaded(dataset) {
+    console.log("dataset: ", dataset)
+    if (dataset === 'target-rnaseq') {
+      this.filterFields = this.targetFields;
+    } else if (dataset === 'tcga-rnaseq') {
+      this.filterFields = this.tcgaFields;
+    }
+
+    if (dataset.length > 0) {
+      this.queryString = this.buildFacetFieldQueryString(this.filterFields, dataset);
+      this.updateFilterValues();
+    }
   }
 
   buildFacetFieldQueryString(categoryArray, dataset) {
@@ -45,16 +65,15 @@ export class PublicDatasetsComponent implements OnInit {
     return query;
   }
 
-  buildFacetFieldQueryRangeString(category, dataset, start, end, gap) {
-    let query = `${this.API_URL}/public-datasets/query/${dataset}/?q=*&facet=true`;
-    query += `&facet.field=${category}&facet.range=${category}&facet.range.start=${start}&facet.range.end=${end}&facet.range.gap=${gap}`;
-    return query;
-  }
+  // buildFacetFieldQueryRangeString(category, dataset, start, end, gap) {
+  //   let query = `${this.API_URL}/public-datasets/query/${dataset}/?q=*&facet=true`;
+  //   query += `&facet.field=${category}&facet.range=${category}&facet.range.start=${start}&facet.range.end=${end}&facet.range.gap=${gap}`;
+  //   return query;
+  // }
 
   getQueryResults(queryString) {
     return this.httpClient.get(queryString)
   }
-
 
   updateFilterValues() {
     this.getQueryResults(this.queryString)
@@ -72,19 +91,17 @@ export class PublicDatasetsComponent implements OnInit {
   }
 
   onChecked(currResult, cat, subcat) {
-    let newQueryItem = `${cat}:${subcat}`;
+    let newQueryItem = `${cat}:"${subcat}"`;
     if (currResult === true) {
       this.checkBoxItems.push(newQueryItem);
     } else {
       this.checkBoxItems = this.checkBoxItems.filter(item => item !== newQueryItem);
     }
-    // console.log("current checked items: ", this.checkBoxItems, this.queryString)
   }
 
   filterData() {
     let newQueryString = this.checkBoxItems.join(' AND ');
-    console.log("new query string: ", newQueryString);
-    this.searchQueryResults = "?q="+ newQueryString;
+    this.searchQueryResults = newQueryString;
   }
 
   setDataset(datasetTag: string) {
--- projects/web-mev/src/app/features/public-datasets-manager/components/target-rnaseq/target-rnaseq.component.ts
+++ projects/web-mev/src/app/features/public-datasets-manager/components/target-rnaseq/target-rnaseq.component.ts
@@ -12,6 +12,9 @@ import { PublicDatasetService } from '../../services/public-datasets.service';
 import { GdcRnaseqComponent } from '../gdc_base/gdc-base.component';
 import { MatDialog } from '@angular/material/dialog';
 import { forkJoin } from 'rxjs';
+import {PublicDatasetsComponent} from '../public-datasets-container/public-datasets.component';
+import { PublicDatasetExportNameDialogComponent } from '../export-name-dialog/export-name-dialog.component';
+
 
 @Component({
   selector: 'target-rnaseq-explorer',
@@ -19,33 +22,38 @@ import { forkJoin } from 'rxjs';
   styleUrls: ['./target-rnaseq.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TargetRnaseqComponent extends GdcRnaseqComponent implements OnChanges {
+export class TargetRnaseqComponent extends GdcRnaseqComponent implements OnChanges, OnInit {
   @Input() query: string = "placeholder for nowx"
   datasetTag = 'target-rnaseq';
   name_map_key = 'target_type_to_name_map';
   tissue_types_url = '';
+  types_url ='';
 
   constructor(
     public cdRef: ChangeDetectorRef,
     public pdService: PublicDatasetService,
     public notificationService: NotificationService,
     public fileService: FileService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    public publicDataSetComponent: PublicDatasetsComponent
   ) {
     super(cdRef, pdService, notificationService, fileService, dialog);
   }
 
   ngOnChanges(): void {
-    this.fetchData2(this.datasetTag, this.name_map_key);
+    this.fetchDataTarget(this.datasetTag, this.name_map_key);
   }
+
+  ngOnInit(): void {
+      this.publicDataSetComponent.afterLoaded(this.datasetTag)
+  }
+  
 
   createDataset(dataType: string) {
-    this._createDataset(dataType, this.datasetTag);
+    this._createDatasetTarget(dataType, this.datasetTag);
   }
 
-  fetchData2(datasetTag: string, name_map_key: string): void {
-    console.log("this is running fetch data 2: ", this.query)
-
+  fetchDataTarget(datasetTag: string, name_map_key: string): void {
     // this object ties together multiple requests so that all the data can be combined 
     // at once. The prepare the TCGA display we need two sources of info-
     // 1. the faceted results detailing how many samples are available in each cancer type
@@ -54,9 +62,11 @@ export class TargetRnaseqComponent extends GdcRnaseqComponent implements OnChang
     // the interface, we need both
     let $observable_dict = {};
     let facet_list = [];
-    let types_url = datasetTag + '/?q=*:*&facet=on&facet.field=project_id&rows=0' + `&${this.query}`
-    // let types_url = 'target-rnaseq/?q=gender:female&vital_status:Alive&facet=on&facet.field=tissue_or_organ_of_origin'
-    $observable_dict['solr_query'] = this.pdService.makeSolrQuery(types_url);
+
+    let originalTypeQueryURL = 'target-rnaseq/?q=*:*&facet=on&facet.field=project_id&rows=0';
+    this.types_url = (this.query.length === 0) ? originalTypeQueryURL : `${this.datasetTag}/?q=${this.query}&facet=on&facet.field=project_id`;
+    
+    $observable_dict['solr_query'] = this.pdService.makeSolrQuery(this.types_url);
     $observable_dict['db_query'] = this.pdService.getPublicDatasetDetails(datasetTag);
     forkJoin($observable_dict).subscribe(
       results => {
@@ -83,17 +93,10 @@ export class TargetRnaseqComponent extends GdcRnaseqComponent implements OnChang
         this.cdRef.markForCheck();
       }
     );
-    let addTissueFacet = '&facet=on&facet.field=tissue_or_organ_of_origin'
-    // let tissue_types_url = datasetTag + '?q=*:*&facet=on&facet.field=tissue_or_organ_of_origin&rows=0' + `&${this.query}`;
-    // let tissue_types_url = 'target-rnaseq/?q=race:asian AND gender:female&facet=on&facet.field=tissue_or_organ_of_origin'
+    
+    let originalTissueQueryURL = 'target-rnaseq/?q=*:*&facet=on&facet.field=tissue_or_organ_of_origin&rows=0';
+    this.tissue_types_url = (this.query.length === 0) ? originalTissueQueryURL : `${this.datasetTag}/?q=${this.query}&facet=on&facet.field=tissue_or_organ_of_origin`;
 
-    if (this.query.length === 0) {
-      this.tissue_types_url = 'target-rnaseq/?q=*:*&facet=on&facet.field=tissue_or_organ_of_origin&rows=0'
-    } else {
-      this.tissue_types_url = this.datasetTag + "/" + this.query + addTissueFacet;
-      console.log("used the filtered url: ", this.tissue_types_url)
-    }
-    // console.log("tissue type url: ", tissue_types_url)
     this.pdService.makeSolrQuery(this.tissue_types_url).subscribe(
       data => {
         let facet_list = [];
@@ -115,4 +118,97 @@ export class TargetRnaseqComponent extends GdcRnaseqComponent implements OnChang
     );
 
   }
+
+  _createDatasetTarget(dataType: string, datasetTag: string){
+    console.log("from createdataset: ", dataType, datasetTag, this.selectedNames, this.query)
+    let $observable_dict = {};
+
+    const dialogRef = this.dialog.open(PublicDatasetExportNameDialogComponent, {disableClose: true});
+  
+    // we add the observable returned by `afterClosed` so that we can simultaneously query the backend
+    // for sample IDs corresponding to the users selection(s) PLUS get any custom name they provide
+    // for this data export. By putting all these observables into an object, we can then use the forkJoin
+    // method below to ensure everything is prepared to send the final request which will create the new file(s)
+    $observable_dict['output_name'] = dialogRef.afterClosed();
+    this.cdRef.markForCheck();
+    let url_suffix = '';
+    // will have type (TCGA, TARGET, etc. identifier) (or tissue) addressing an Observable
+    if(dataType === 'byType'){
+        for(let i in this.selectedNames){
+            let type_id = this.selectedNames[i];
+            let count = this.type_count_dict[type_id];
+            url_suffix = datasetTag + `?q=project_id:"${type_id}"  AND ${this.query}&rows=${count}&fl=id`;
+            console.log("url suffix: ", url_suffix)
+            $observable_dict[type_id] = this.pdService.makeSolrQuery(url_suffix);
+        }
+    } else if(dataType === 'byTissue'){
+        for(let i in this.selectedNames){
+            let tissue_name = this.selectedNames[i];
+            let count = this.tissue_count_dict[tissue_name];
+            // since the data is organized (on the backend) into a structured HDFS
+            // using the "cancer type" as the key, we need to also find out which 
+            // "project" each of these tissues corresponds to. We get that by adding
+            // to the fl=... param
+            url_suffix = datasetTag + `?q=tissue_or_organ_of_origin:"${tissue_name}" AND ${this.query}&rows=${count}&fl=id,project_id`;
+            console.log("url suffix: ", url_suffix)
+            $observable_dict[tissue_name] = this.pdService.makeSolrQuery(url_suffix);
+        }
+    }
+    // forkJoin forces all the observables to complete and then takes
+    // their results. If we don't do this, the subsequent request to
+    // create the dataset can be 'waiting' for the list of sample IDs
+    // and hence the request for dataset creation will be incomplete/invalid.
+    forkJoin($observable_dict).subscribe(
+        results => {
+            let datasetName = results['output_name'];
+            delete results['output_name'];
+
+            // a value of `null` here is a signal that the user aborted
+            // creation of the dataset via the modal (the one which asks
+            // them to provide a custom name for the data export). Hence, we 
+            // bail if a null is encountered
+            if (datasetName === null ){
+                return;
+            }
+            let filter_payload = {};
+            let doc_list = [];
+            Object.keys(results).forEach(key => {
+                doc_list = results[key]['response']['docs'];
+                if(dataType === 'byType'){
+                    filter_payload[key] = doc_list.map(doc => doc['id']);
+                } else {
+                    // queried by tissue. To make a proper request to the endpoint, 
+                    // we need to map the samples to their "types" (e.g. TCGA cancer type)
+                    // rather than map the sample IDs via their tissue.
+                    for(let doc_idx in doc_list){
+                        let doc = doc_list[doc_idx];
+                        let id = doc['id'];
+                        let project_id = doc['project_id'];
+                        if (filter_payload.hasOwnProperty(project_id) ){
+                            filter_payload[project_id].push(id);
+                        } else {
+                            filter_payload[project_id] = [id];
+                        }
+                    }
+                }
+            });
+            let final_payload = {
+                'filters': filter_payload,
+                'output_name': datasetName
+            };
+            this.isWaiting = true;
+            this.cdRef.markForCheck();
+            this.pdService.createDataset(datasetTag,final_payload).subscribe(
+                results => {
+                    this.isWaiting = false;
+                    this.cdRef.markForCheck();
+                    this.fileService.getAllFilesPolled();
+                    this.notificationService.success('Your files are being prepared.' +
+                        ' You can check the status of these in the file browser.'
+                    );
+                }
+            );  
+        }    
+    );
+}
 }

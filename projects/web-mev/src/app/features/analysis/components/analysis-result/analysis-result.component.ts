1e53076df37e9ce6b46d52de12233425ff777e09
Saron Nhong <saronnhong@gmail.com>
2022-06-28 09:55:49 -0400

    remove console logs

--- projects/web-mev/src/app/features/analysis/components/analysis-result/analysis-result.component.ts
+++ projects/web-mev/src/app/features/analysis/components/analysis-result/analysis-result.component.ts
@@ -23,7 +23,6 @@ export class AnalysisResultComponent implements OnChanges {
   ngOnChanges(): void {
     this.outputs = { ...this.outputs };
     this.operationName = this.getOperationName();
-    console.log("operations name: ", this.operationName)
   }
 
   /**

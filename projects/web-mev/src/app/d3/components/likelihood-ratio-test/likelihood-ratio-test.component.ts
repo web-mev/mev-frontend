3618ea31c459dd37b6bda205313918b23f975196
Saron Nhong <saronnhong@gmail.com>
2022-11-18 15:07:59 -0800

    likelihood ratio test, data saved for boxplots

--- projects/web-mev/src/app/d3/components/likelihood-ratio-test/likelihood-ratio-test.component.ts
+++ projects/web-mev/src/app/d3/components/likelihood-ratio-test/likelihood-ratio-test.component.ts
@@ -1,9 +1,9 @@
 import { Component, ChangeDetectionStrategy, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
-import { NotificationService } from '@core/notifications/notification.service';
-import * as d3 from 'd3';
-import d3Tip from 'd3-tip';
+// import { NotificationService } from '@core/notifications/notification.service';
+// import * as d3 from 'd3';
+// import d3Tip from 'd3-tip';
 import { catchError } from 'rxjs/operators';
 
 @Component({
@@ -21,26 +21,44 @@ export class LikelihoodRatioTestComponent implements OnInit {
     displayedColumns: string[] = ['gene', 'baseMean', 'log2FoldChange', 'statistic', 'pvalue', 'padj'];
     dataSource = [];
     pageIndex = 0;
-    limit = 25;
+    limit = 10;
     lfc_comparison = '';
     private readonly API_URL = environment.apiUrl;
+    sampleIdToGroup = {};
+    boxplotData = {};
+    boxplotData_test = [1,2,3,4,5,6,7,7,4,3,23,66,2,3]
 
     constructor(
         private httpClient: HttpClient,
-        private readonly notificationService: NotificationService,
+        // private readonly notificationService: NotificationService,
     ) { }
 
 
     ngOnInit(): void {
         console.log("outputs: ", this.outputs)
+        this.createSample2GroupDict()
         this.lfc_comparison = this.outputs['lfc_comparison']
         let dge_results = this.outputs['dge_results'];
         this.getData(dge_results);
+
     }
+
+    createSample2GroupDict() {
+        let categories = this.outputs['sample_to_group_mapping']
+        for (let item in categories) {
+            let categoryArr = categories[item]
+            for (let index in categoryArr) {
+                this.sampleIdToGroup[categoryArr[index]] = item
+            }
+        }
+        console.log("sampleID to Group: ", this.sampleIdToGroup)
+    }
+
+
 
     getData(uuid) {
         this.isLoading = true;
-        let queryURL = `${this.API_URL}/resources/${uuid}/contents/`
+        let queryURL = `${this.API_URL}/resources/${uuid}/contents/`;
         this.httpClient.get(queryURL).pipe(
             catchError(error => {
                 console.log("Error: ", error.message);
@@ -48,6 +66,7 @@ export class LikelihoodRatioTestComponent implements OnInit {
                 throw message
             }))
             .subscribe(data => {
+                console.log("data: ", data)
                 this.isLoading = false;
                 for (let i in data) {
                     let temp = {
@@ -61,7 +80,24 @@ export class LikelihoodRatioTestComponent implements OnInit {
                     }
                     this.dataSource.push(temp)
                 }
-                console.log("datasource: ", this.dataSource)
+                for (let i in data) {
+                    let rowname = data[i]['rowname']
+                    if (this.boxplotData[rowname] === undefined) {
+                        this.boxplotData[rowname] = {}
+                    }
+
+                    for (let index in data[i]['values']) {
+                        if (this.sampleIdToGroup[index] !== undefined) {
+                            let cat = this.sampleIdToGroup[index]
+                            if (this.boxplotData[rowname][cat] === undefined) {
+                                this.boxplotData[rowname][cat] = [];
+                            }
+                            let value = data[i]['values'][index]
+                            this.boxplotData[rowname][cat].push(value)
+                        }
+                    }
+                }
+                console.log("boxplot data: ", this.boxplotData)
 
             });
     }

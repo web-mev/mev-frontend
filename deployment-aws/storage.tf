e16811a4ea9e2a5519e9b218877f362723cf965d
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-02-28 16:49:02 -0500

    adding a custom catch to properly associate urls and angular routes. without this, unhashed urls will fail in cloudfront

--- deployment-aws/storage.tf
+++ deployment-aws/storage.tf
@@ -11,14 +11,14 @@ resource "aws_s3_bucket_acl" "website" {
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = [
+        Resource = [
           aws_s3_bucket.website.arn,
           "${aws_s3_bucket.website.arn}/*",
         ]
@@ -72,5 +72,14 @@ resource "aws_cloudfront_distribution" "website" {
     acm_certificate_arn      = var.https_cert_arn
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
+  }
+  # this block allows "direct" urls.
+  # Without this, attempts to access {var.website_hostname}/foo
+  # will generate a 404. By adding this, the angular app will
+  # properly load the route associated with /foo
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
   }
 }

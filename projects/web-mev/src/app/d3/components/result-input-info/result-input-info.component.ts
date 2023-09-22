f5b950b7d760ff5a89c302ab7a88815fe0da478f
Saron Nhong <saronnhong@gmail.com>
2023-09-22 14:03:31 -0700

    fix results input info page

--- projects/web-mev/src/app/d3/components/result-input-info/result-input-info.component.ts
+++ projects/web-mev/src/app/d3/components/result-input-info/result-input-info.component.ts
@@ -18,13 +18,12 @@ export class ResultInputInfoComponent implements OnInit {
 
   ngOnInit(): void {
     this.getInputValues()
+    
   }
 
   getInputValues() {
     console.log("result input info: ", this.outputs)
     let excludeList = ['operation', 'job_name', 'error_messages']
-
-    // let inputList = {}
 
     for (let key in this.outputs) {
       if (!excludeList.includes(key)) {
@@ -40,7 +39,6 @@ export class ResultInputInfoComponent implements OnInit {
         if (value.length === 36) {
           let query = `https://dev-mev-api.tm4.org/api/resources/${value}/`
           this.httpClient.get(query).subscribe(res => {
-            // console.log(`${key}: `, res['name'], res)
             this.inputList[formattedText] = res['name']
           }),
             (error) => {
@@ -54,21 +52,22 @@ export class ResultInputInfoComponent implements OnInit {
       }
 
     }
-    console.log("input list: ", this.inputList)
-    //get these values from analysis-result and query the api for the name of the file
-    // let input_matrix_a = "e85b5f38-f6a9-4ad2-a8d0-2b7cba1d4328"
-    // // let input_matrix_b = "1f8fb6e2-15f3-4dea-9b81-23f2f8dfce2b"
-    // let input_matrix_b = "d3c3754c-43d6-49ff-843b-1b484e5c749f"
-    // let sample_arrangement = "sample ID"
-    // let query_a = `https://dev-mev-api.tm4.org/api/resources/${input_matrix_a}/`
-    // let query_b = `https://dev-mev-api.tm4.org/api/resources/${input_matrix_b}/`
-    // this.httpClient.get(query_a).subscribe(res => {
-    //     console.log("input_a: ", res['name'], res)
-    // })
-    // this.httpClient.get(query_b).subscribe(res => {
-    //     console.log("input_b: ", res['name'])
-    // })
+    // this.updateTooltipContent()
   }
+
+  // tooltipContent = ''
+  // newString = 'hello '
+
+  // updateTooltipContent() {
+  //   console.log("input list: ", this.inputList)
+  //   // let newString = ''
+  //   for(let key in this.inputList){
+  //     let temp = `${key}: ${this.inputList[key]} `
+  //     this.newString = this.newString + temp + " &#13; "
+  //   }
+  //   this.tooltipContent = this.newString;
+  //   console.log("when finish: ", this.newString)
+  // }
 
 
 }

a4a60fa54430fd9d40846cd76d3028ef6fe1a8e4
Brian Lawney <brianlawney@hsph.harvard.edu>
2024-04-30 16:27:27 -0400

    functionally complete. needs styling improvements

--- projects/web-mev/src/app/features/analysis/components/differential-expression-input/differential-expression-input.component.ts
+++ projects/web-mev/src/app/features/analysis/components/differential-expression-input/differential-expression-input.component.ts
@@ -1,124 +1,243 @@
 import {
-  Component,
-  ChangeDetectionStrategy,
-  OnChanges,
-  Output,
-  EventEmitter,
-  Input
+    Component,
+    ChangeDetectionStrategy,
+    OnChanges,
+    Output,
+    EventEmitter,
+    Input
 } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { FormGroup, Validators, FormBuilder, AbstractControl, AbstractControlOptions, ValidationErrors, ValidatorFn, AsyncValidatorFn } from '@angular/forms';
 import { AnalysesService } from '../../services/analysis.service';
 import { MetadataService } from '@app/core/metadata/metadata.service';
 import { MatDialog } from '@angular/material/dialog';
-import { NotificationService } from '@core/notifications/notification.service';
 import { BaseOperationInput } from '../base-operation-inputs/base-operation-inputs';
+import { CompatibleObsSetService } from '../../services/compatible_obs_set.service';
+
+
+// checks that the chosen observation sets are 1) not the same and 2) have no intersection
+const intersectingObservationSetsValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const obs_set_1_ctrl = control.get('base_condition_samples');
+    const obs_set_2_ctrl = control.get('experimental_condition_samples');
+    const obs_set_1 = obs_set_1_ctrl.value;
+    const obs_set_2 = obs_set_2_ctrl.value;
+    if (obs_set_1 && obs_set_2) {
+
+        if (obs_set_1.name === obs_set_2.name) {
+            return { sameName: true }
+        }
+
+        const v1 = obs_set_1.elements.map(item => {
+            return item.id;
+        });
+        const v2 = obs_set_2.elements.map(item => {
+            return item.id;
+        })
+        const elements_1 = new Set(v1);
+        const elements_2 = new Set(v2);
+        let intersection = [];
+        for (const v of elements_1) {
+            if (elements_2.has(v)) {
+                intersection.push(v)
+            }
+        }
+        if (intersection.length > 0) {
+            return { intersection: intersection }
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
 
 /**
  * Used to display interactive inputs for a differential expression 
  * style analysis
  */
 @Component({
-  selector: 'differential-expression-input',
-  templateUrl: './differential-expression-input.component.html',
-  styleUrls: ['./differential-expression-input.component.scss'],
-  providers: [{ provide: BaseOperationInput, useExisting: DifferentialExpressionInputComponent }],
-  changeDetection: ChangeDetectionStrategy.Default
+    selector: 'differential-expression-input',
+    templateUrl: './differential-expression-input.component.html',
+    styleUrls: ['./differential-expression-input.component.scss'],
+    providers: [{ provide: BaseOperationInput, useExisting: DifferentialExpressionInputComponent }],
+    changeDetection: ChangeDetectionStrategy.Default
 })
 export class DifferentialExpressionInputComponent extends BaseOperationInput implements OnChanges {
-  analysesForm: FormGroup;
-  submitted = false;
-  @Input() operationData: any;
-  @Input() workspaceId: string;
-  @Output() formValid: EventEmitter<any> = new EventEmitter<any>();
+    analysesForm: FormGroup;
+    submitted = false;
+    @Input() operationData: any;
+    @Input() workspaceId: string;
+    @Output() formValid: EventEmitter<any> = new EventEmitter<any>();
 
-  rawCountsField;
+    rawCountsField;
+    ctrlObsSetField;
+    experimentalObsSetField;
+    baseConditionNameField;
+    experimentalConditionNameField;
+    rawCountsSamples = [];
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private apiService: AnalysesService,
-    private metadataService: MetadataService,
-    public dialog: MatDialog,
-    private readonly notificationService: NotificationService
-  ) {
-    super();
-  }
-
-  ngOnChanges(): void {
-    if (this.operationData) {
-      this.createForm();
-      this.analysesForm.statusChanges.subscribe(() => this.onFormValid());
+    constructor(
+        private formBuilder: FormBuilder,
+        private apiService: AnalysesService,
+        private metadataService: MetadataService,
+        public dialog: MatDialog,
+        private obsSetService: CompatibleObsSetService
+    ) {
+        super();
     }
-  }
 
-  getInputData(): any {
-    let inputs = this.convertToFloatObj(this.analysesForm.value);
-    return inputs;
-  }
+    ngOnChanges(): void {
+        if (this.operationData) {
+            this.createForm();
+            this.analysesForm.statusChanges.subscribe(() => this.onFormValid());
+        }
+    }
 
-  onSubmit() {
-    this.submitted = true;
-  }
+    getInputData(): any {
+        let inputs = this.convertToFloatObj(this.analysesForm.value);
+        return inputs;
+    }
 
-  public onFormValid() {
-    this.formValid.emit(this.analysesForm.valid);
-  }
+    onSubmit() {
+        this.submitted = true;
+    }
 
-  createForm() {
-    const controlsConfig = {};
-    let input;
+    public onFormValid() {
+        this.formValid.emit(this.analysesForm.valid);
+    }
 
-    // the job name field:
-    controlsConfig['job_name'] = ['', [Validators.required]];
+    createForm() {
+        const controlsConfig = {};
+        let input;
 
-    // the selection for the raw counts:
-    let key = "raw_counts"
-    input = this.operationData.inputs[key];
-    console.log(input);
-    this.rawCountsField = {
-      key: key,
-      name: input.name,
-      resource_types: input.spec.resource_types,
-      desc: input.description,
-      required: input.required,
-      files: [],
-      selectedFiles: []
-    };
-    this.apiService
-      .getWorkspaceResourcesByParam(
-        input.spec.resource_types,
-        this.workspaceId
-      )
-      .subscribe(data => {
-        this.rawCountsField.files = data;
-      });
+        // the job name field:
+        controlsConfig['job_name'] = ['', [Validators.required]];
 
-    const configResourceField = [
-      '',
-      [...(input.required ? [Validators.required] : [])]
-    ];
-    controlsConfig[key] = configResourceField;
-    this.analysesForm = this.formBuilder.group(controlsConfig);
-  }
+        // the selection for the raw counts:
+        let key = "raw_counts"
+        input = this.operationData.inputs[key];
+        console.log(input);
+        this.rawCountsField = {
+            key: key,
+            name: input.name,
+            resource_types: input.spec.resource_types,
+            desc: input.description,
+            required: input.required,
+            files: [],
+            selectedFiles: []
+        };
 
-  /**
-    * Convenience getter for easy access to form fields
-    */
+        this.apiService
+            .getWorkspaceResourcesByParam(
+                input.spec.resource_types,
+                this.workspaceId
+            )
+            .subscribe(data => {
+                this.rawCountsField.files = data;
+            });
+
+        const configResourceField = [
+            '',
+            [Validators.required]
+        ];
+        controlsConfig[key] = configResourceField;
+
+        // add the observation sets. Note that the obs sets cannot contain
+        // a non-null intersection.
+
+        // get all the available obs sets
+        const availableObsSets = this.metadataService.getCustomObservationSets().map(set => {
+            const newSet = set.elements.map(elem => {
+                const o = { id: elem.id };
+                return o;
+            });
+            return { ...set, elements: newSet };
+        });
+        key = 'base_condition_samples'
+        input = this.operationData.inputs[key];
+        this.ctrlObsSetField = {
+            key: key,
+            name: input.name,
+            desc: input.description,
+            required: input.required,
+            sets: availableObsSets
+        };
+        const configBaseObservationSetsField = [
+            undefined,
+            [...(input.required ? [Validators.required] : [])]
+        ];
+        controlsConfig[key] = configBaseObservationSetsField;
+
+        key = 'experimental_condition_samples'
+        input = this.operationData.inputs[key];
+        this.experimentalObsSetField = {
+            key: key,
+            name: input.name,
+            desc: input.description,
+            required: input.required,
+            sets: availableObsSets
+        };
+        const configExpObservationSetsField = [
+            undefined,
+            [...(input.required ? [Validators.required] : [])]
+        ];
+        controlsConfig[key] = configExpObservationSetsField;
+
+        key = 'base_condition_name'
+        input = this.operationData.inputs[key];
+        this.baseConditionNameField = {
+            key: key,
+            name: input.name,
+            desc: input.description,
+            required: input.required
+        };
+        const configBaseConditionNameField = [
+            '',
+            [...(input.required ? [Validators.required] : [])]
+        ];
+        controlsConfig[key] = configBaseConditionNameField;
+
+        key = 'experimental_condition_name'
+        input = this.operationData.inputs[key];
+        this.experimentalConditionNameField = {
+            key: key,
+            name: input.name,
+            desc: input.description,
+            required: input.required
+        };
+        const configExperimentalConditionNameField = [
+            '',
+            [...(input.required ? [Validators.required] : [])]
+        ];
+        controlsConfig[key] = configExperimentalConditionNameField;
+
+        this.analysesForm = this.formBuilder.group(controlsConfig,
+            {
+                validators: [intersectingObservationSetsValidator],
+                asyncValidators: [this.obsSetService.validate()]
+            } as AbstractControlOptions
+        );
+    }
+
+    /**
+      * Convenience getter for easy access to form fields
+      */
     get f() {
-    return this.analysesForm.controls;
-  }
+        return this.analysesForm.controls;
+    }
 
-  /**
-    * Function is used to convert strings to floats for numeric values
-    * before sending the data to execute the operation
-    */
-  convertToFloatObj(obj) {
-      const res = {};
-      for (const key in obj) {
-          res[key] =
-              isNaN(obj[key]) || typeof obj[key] === 'boolean'
-                  ? obj[key]
-                  : parseFloat(obj[key]);
-      }
-      return res;
-  }
+    /**
+      * Function is used to convert strings to floats for numeric values
+      * before sending the data to execute the operation
+      */
+    convertToFloatObj(obj) {
+        const res = {};
+        for (const key in obj) {
+            res[key] =
+                isNaN(obj[key]) || typeof obj[key] === 'boolean'
+                    ? obj[key]
+                    : parseFloat(obj[key]);
+        }
+        return res;
+    }
 }
---
+++ projects/web-mev/src/app/features/analysis/services/compatible_obs_set.service.ts
@@ -0,0 +1,52 @@
+import { AbstractControl, ValidationErrors, AsyncValidatorFn } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { FileService } from '@file-manager/services/file-manager.service';
+import { map, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CompatibleObsSetService {
+
+    constructor(private fileService: FileService) { }
+
+    validate(): AsyncValidatorFn {
+        return (control: AbstractControl): Observable<ValidationErrors | null> => {
+            const obs_set_1_ctrl = control.get('base_condition_samples');
+            const obs_set_2_ctrl = control.get('experimental_condition_samples');
+            const exp_mtx_ctrl = control.get('raw_counts');
+            const obs_set_1 = obs_set_1_ctrl.value;
+            const obs_set_2 = obs_set_2_ctrl.value;
+            const raw_counts = exp_mtx_ctrl.value;
+            if (obs_set_1 && obs_set_2 && raw_counts) {
+                return this.fileService.getFilePreview(raw_counts).pipe(
+                    map(data => {
+                        const firstRow = new Set(Object.keys(data[0].values));
+
+                        const v1 = obs_set_1.elements.map(item => {
+                            return item.id;
+                        });
+                        const v2 = obs_set_2.elements.map(item => {
+                            return item.id;
+                        })
+                        const requested_samples = new Set([...v1, ...v2]);
+                        let bad_samples = [];
+                        for (const v of requested_samples) {
+                            if (!firstRow.has(v)) {
+                                bad_samples.push(v);
+                            }
+                        }
+                        if (bad_samples.length > 0) {
+                            return { badSample: bad_samples }
+                        } else {
+                            return null
+                        }
+                    })
+                );
+            } else {
+                return of(null)
+            }
+        }
+    }
+}
